66e3487f48c7e1c2c8aca6da5916dcd73258d041
Toseeb Nadaf <toseebnadaf@gmail.com>
2023-07-26 20:30:36 +0530

    Created /etc/hosts entry pointing to localhost

---
+++ hosts_entry_create.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+def check_installed(package_name):
+    try:
+        subprocess.check_output([package_name, "--version"])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except OSError:
+        return False
+
+def install_package(package_name):
+    try:
+        subprocess.check_call(["sudo", "apt-get", "install", "-y", package_name])
+        print("{} installed successfully.".format(package_name))
+    except subprocess.CalledProcessError:
+        print("Failed to install {}. Please check the installation manually.".format(package_name))
+
+def create_wordpress_site(site_name):
+    try:
+        subprocess.check_call(["docker", "run", "-d", "--name", site_name, "-p", "80:80", "wordpress"])
+        print("WordPress site '{}' created successfully.".format(site_name))
+    except subprocess.CalledProcessError:
+        print("Failed to create the WordPress site. Please check the installation and try again.")
+
+def add_etc_hosts_entry(site_name):
+    try:
+        with open('/etc/hosts', 'a') as hosts_file:
+            hosts_file.write('\n127.0.0.1\t{}\n'.format(site_name))
+        print("Added /etc/hosts entry for {}.".format(site_name))
+    except PermissionError:
+        print("Failed to add /etc/hosts entry. Please run the script with sudo or as an administrator.")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: {} <site_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    site_name = sys.argv[1]
+
+    required_packages = ["docker", "docker-compose"]
+
+    for package in required_packages:
+        if not check_installed(package):
+            print("{} is not installed.".format(package))
+            install_package(package)
+
+    create_wordpress_site(site_name)
+    add_etc_hosts_entry(site_name)
+
+if __name__ == "__main__":
+    main()
