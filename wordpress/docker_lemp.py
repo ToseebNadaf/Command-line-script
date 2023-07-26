2ca3ab54077830efbc4f39fad8d3ec852e5fe808
Toseeb Nadaf <toseebnadaf@gmail.com>
2023-07-26 21:14:29 +0530

    Added functionality to enable/disable/delete containers, local-files through command-line

--- wordpress/docker_lemp.py
+++ wordpress/docker_lemp.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import sys
+import os
+import webbrowser
 
 def check_installed(package_name):
     try:
@@ -19,6 +21,42 @@ def install_package(package_name):
     except subprocess.CalledProcessError:
         print("Failed to install {}. Please check the installation manually.".format(package_name))
 
+def create_wordpress_site(site_name):
+    try:
+        subprocess.check_call(["docker", "run", "-d", "--name", site_name, "-p", "80:80", "wordpress"])
+        print("WordPress site '{}' created successfully.".format(site_name))
+    except subprocess.CalledProcessError:
+        print("Failed to create the WordPress site. Please check the installation and try again.")
+
+def add_etc_hosts_entry(site_name):
+    try:
+        with open('/etc/hosts', 'a') as hosts_file:
+            hosts_file.write('\n127.0.0.1\t{}\n'.format(site_name))
+        print("Added /etc/hosts entry for {}.".format(site_name))
+    except PermissionError:
+        print("Failed to add /etc/hosts entry. Please run the script with sudo or as an administrator.")
+
+def remove_etc_hosts_entry(site_name):
+    try:
+        with open('/etc/hosts', 'r') as hosts_file:
+            lines = hosts_file.readlines()
+
+        with open('/etc/hosts', 'w') as hosts_file:
+            for line in lines:
+                if site_name not in line:
+                    hosts_file.write(line)
+
+        print("Removed /etc/hosts entry for {}.".format(site_name))
+    except PermissionError:
+        print("Failed to remove /etc/hosts entry. Please run the script with sudo or as an administrator.")
+
+def prompt_open_in_browser(site_name):
+    try:
+        webbrowser.open('http://{}'.format(site_name))
+        print("Please visit to http://{}".format(site_name))
+    except webbrowser.Error:
+        print("Unable to open the browser. Please manually open 'http://{}'.".format(site_name))
+
 def start_lemp_stack():
     try:
         subprocess.check_call(["docker-compose", "up", "-d"])
@@ -26,7 +64,49 @@ def start_lemp_stack():
     except subprocess.CalledProcessError:
         print("Failed to start LEMP stack containers. Please check the setup and try again.")
 
+def stop_lemp_stack():
+    try:
+        subprocess.check_call(["docker-compose", "down"])
+        print("LEMP stack containers stopped successfully.")
+    except subprocess.CalledProcessError:
+        print("Failed to stop LEMP stack containers. Please check the setup and try again.")
+
+def delete_wordpress_site(site_name):
+    try:
+        subprocess.check_call(["docker-compose", "down", "--volumes"])
+        print("LEMP stack containers and volumes for site '{}' deleted successfully.".format(site_name))
+    except subprocess.CalledProcessError:
+        print("Failed to delete LEMP stack containers and volumes. Please check the setup and try again.")
+
+def delete_etc_hosts_entry(site_name):
+    try:
+        with open('/etc/hosts', 'r') as hosts_file:
+            lines = hosts_file.readlines()
+
+        with open('/etc/hosts', 'w') as hosts_file:
+            for line in lines:
+                if site_name not in line:
+                    hosts_file.write(line)
+
+        print("Removed /etc/hosts entry for {}.".format(site_name))
+    except PermissionError:
+        print("Failed to remove /etc/hosts entry. Please run the script with sudo or as an administrator.")
+
+def delete_local_files(site_name):
+    try:
+        subprocess.check_call(["sudo", "rm", "-rf", site_name])
+        print("Local files for site '{}' deleted successfully.".format(site_name))
+    except subprocess.CalledProcessError:
+        print("Failed to delete local files. Please check the setup and try again.")
+
 def main():
+    if len(sys.argv) < 3:
+        print("Usage: {} <subcommand> <site_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    subcommand = sys.argv[1]
+    site_name = sys.argv[2]
+
     required_packages = ["docker", "docker-compose"]
 
     for package in required_packages:
@@ -34,8 +114,26 @@ def main():
             print("{} is not installed.".format(package))
             install_package(package)
 
-    start_lemp_stack()
+    if subcommand == "enable":
+        create_wordpress_site(site_name)
+        add_etc_hosts_entry(site_name)
+        start_lemp_stack()
+        print("Site '{}' enabled successfully.".format(site_name))
+        prompt_open_in_browser(site_name)
+
+    elif subcommand == "disable":
+        stop_lemp_stack()
+        remove_etc_hosts_entry(site_name)
+        print("Site '{}' disabled successfully.".format(site_name))
+
+    elif subcommand == "delete":
+        delete_wordpress_site(site_name)
+        remove_etc_hosts_entry(site_name)
+        delete_local_files(site_name)
+        print("Site '{}' deleted successfully.".format(site_name))
+
+    else:
+        print("Invalid subcommand. Supported subcommands: 'enable' or 'disable'.")
 
 if __name__ == "__main__":
     main()
-
